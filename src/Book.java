6027efd932bd1ebee84b66bf0bc2143b59810048
Said Elarays <saidelarays@hotmail.fr>
2019-12-11 22:38:22 +0100

    v1

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+
+    static String auteur,titre,ISBN;
+    static int prix;
+
+    public Book(int prix,String auteur,String ISBN,String titre) {
+        this.prix=prix;
+        this.auteur=auteur;
+        this.ISBN=ISBN;
+        this.titre=titre;
+    }
+
+
+}
---
+++ src/Inventory.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Inventory {
+
+        public ArrayList<Object> inventaire;
+
+    public Inventory() {
+        inventaire = new ArrayList<Object>();
+    }
+
+    public int get(Object n){
+        
+    }
+
+    public Object[] getAll(){}
+
+    public void add(Object e,int n){}
+
+    public void remove (Object e,int n){}
+
+
+
+}
