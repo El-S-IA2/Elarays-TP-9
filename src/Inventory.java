5c7c6d791fe04f6a14f1c1b1e340983ce1e2e846
Elarays <56716561+Elarays@users.noreply.github.com>
2019-12-12 12:13:45 +0100

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,23 +1,63 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Inventory {
 
-        public ArrayList<Object> inventaire;
+    public ArrayList<Entry> inventaire;
 
     public Inventory() {
-        inventaire = new ArrayList<Object>();
+        inventaire = new ArrayList<Entry>();
     }
 
     public int get(Object n){
-        
+        Iterator<Entry> it= inventaire.iterator();
+        while(it.hasNext()) {
+            Entry couple = it.next();
+            if (couple.getFirst()==n) {
+                return couple.getSecond();
+            }
+        }
+        return 0;
     }
 
-    public ArrayList<Object> getAll(){}
+    public ArrayList<Object> getAll(){
+        ArrayList<Object> listeElements = new ArrayList<>();
 
-    public void add(Object e,int n){}
+        Iterator<Entry> it= inventaire.iterator();
+        while(it.hasNext()) {
+            Entry couple = it.next();
+            if (couple.getSecond()>0) {
+                listeElements.add(couple.getFirst());
+            }
+        }
+
+        return listeElements;
+
+    }
+
+    public void add(Object e,int n) {
+
+        if (n < 0) {
+            throw new IllegalArgumentException("n negatif");}
+
+            if (inventaire.size() == 0)
+                inventaire.add(new Entry(e, n));
+
+            else {
+                int len = inventaire.size();
+                boolean trouve = false;
+                for (int i = 0; i < len; i++) {
+                    if (inventaire.get(i).getFirst() == e) {
+                        inventaire.get(i).setSecond(n);
+                        trouve = true;
+                    }
+                }
+                if (!trouve)
+                    inventaire.add(new Entry(e, n));
+            }
+        }
+
 
     public void remove (Object e,int n){}
-
-
 
 }
